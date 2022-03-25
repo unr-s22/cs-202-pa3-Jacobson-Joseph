3b065844f006152ec51100aa3e91040f6312dd85
Jacobson-Joseph <josephjacobson@nevada.unr.edu>
2022-03-24 19:39:46 -0700

    Part of the Money Class built

--- Money.cpp
+++ Money.cpp
@@ -1,1 +1,38 @@
 #include "Money.h"
+
+Money::Money(){
+
+}
+
+bool operator < (const Money &rhs) const {
+    return allmoney < rhs.allmoney;
+}
+
+bool operator > (const Money &rhs) const {
+    return allmoney > rhs.allmoney;
+}
+
+bool operator <= (const Money &rhs) const {
+    return allmoney <= rhs.allmoney;
+}
+
+bool operator >= (const Money &rhs) const {
+    return allmoney >= rhs.allmoney;
+}
+
+bool operator != (const Money &rhs) const {
+    return allmoney != rhs.allmoney;
+}
+
+bool operator == (const Money &rhs) const {
+    return allmoney == rhs.allmoney;
+}
+
+bool operator + (const Money &rhs) const {
+    return allmoney + rhs.allmoney;
+}
+
+bool operator - (const Money &rhs) const {
+    return allmoney - rhs.allmoney;
+}
+
--- Money.h
+++ Money.h
@@ -1,6 +1,23 @@
+#include <iostream>
+
 #ifndef MONEY_H
 #define MONEY_H
 
+class Money{
+
+    private:
+        int dollars, cents, allmoney;
+    public:
+    Money();
+
+    friend std::ostream &operator<<(std::ostream &os, const Money &money){
+        os << "$" << money.allmoney * 0.01;
+        return os;
+    }
+
+
+
+};
 
 
 
